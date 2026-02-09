a66a11cd52a0261cc81cd03ce5a3f7d65fac353c
Nicholas Doboszenski <waffleer@orangeSlicer.>
2026-02-09 12:27:30 -0600

    Update comments

--- noxfile.py
+++ noxfile.py
@@ -4,10 +4,10 @@ import nox
 def tests(session):
     """Runs tests with pytest."""
     session.install("pytest")
-    session.run("pytest", "tests") # Runs tests 
+    session.run("pytest", "tests") # Runs tests in tests folder ( files must be named `test_*` )
 
 @nox.session
 def lint(session):
     """Lints the code with flake8."""
     session.install("flake8")
-    session.run("flake8", "pisense", '-v') # Runs linter on backend
+    session.run("flake8", "pisense") # Runs linter on python code in `pisense/*`
