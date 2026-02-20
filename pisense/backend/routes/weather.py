39c47b09b20504217ad77a02005199a2bda98cad
Amber Kuhn <i.am.fastk@gmail.com>
2026-02-20 11:27:56 -0600

    Daily api call

--- pisense/backend/routes/weather.py
+++ pisense/backend/routes/weather.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
-from pisense.backend.models.weather_models import HourlyRecord
+from pisense.backend.models.weather_models import HourlyRecord, DailyRecord
 from pisense.backend.services.weather_service import (
+    get_forecast_day_weather,
     get_forecast_weather,
     get_historical_weather
 )
@@ -22,3 +23,10 @@ async def weather_forecast():
 )
 async def historical_weather():
     return get_historical_weather()
+
+@router.get(
+    "/daily-weather",
+    response_model=list[DailyRecord]
+)
+async def daily_weather():
+    return get_forecast_day_weather()
