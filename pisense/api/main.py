1e6b25352f1f705fa8e760d2781b727c647bd5a8
Amber3344-tech <i.am.fastk@gmail.com>
2026-02-20 10:56:41 -0600

    Modular Weather API set up (#5)
    * modularize api weather endpoints
    
    * fixed modular weather api
    
    * Comment to start
    
    * nox fixes
    
    * nox fix2
    
    * gitignore updated
    
    * Move folder to backend
    
    * fix?
    
    * moved gitignore again
    
    * nox fix

--- pisense/api/main.py
+++ pisense/api/main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+from pisense.backend.routes.weather import router as weather_router
+
+# to start server: source .venv/bin/activate && fastapi dev pisense/api/main.py
 
 app = FastAPI()
+
+app.include_router(weather_router)
 
 
 @app.get("/")
