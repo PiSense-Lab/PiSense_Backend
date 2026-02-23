92489767e91b95275c9cd98b2a8f095ea0d3b5e5
Amber Kuhn <i.am.fastk@gmail.com>
2026-02-23 11:21:19 -0600

    api tests

--- tests/test_weatherapi.py
+++ tests/test_weatherapi.py
@@ -1,5 +1,8 @@
 # import pytest
+from datetime import datetime
+
 from fastapi.testclient import TestClient
+from build.lib.pisense.backend.models.weather_models import HourlyRecord, DailyRecord
 from pisense.api.main import app
 
 client = TestClient(app)
@@ -9,3 +12,38 @@ def test_root():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello World"}
+
+def test_get_historical_weather():
+    response = client.get("/historical-weather")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert isinstance(data, list)
+
+    # Validate each item using Pydantic
+    validated = [HourlyRecord.model_validate(item) for item in data]
+
+    assert len(validated) == len(data)
+
+    # stronger checks
+    for record in validated:
+        assert isinstance(record.date, datetime)
+        assert isinstance(record.temperature_2m, float)
+
+def test_get_daily_weather():
+    response = client.get("/daily-weather")
+    assert response.status_code == 200
+
+    data = response.json()
+    assert isinstance(data, list)
+
+    # Validate each item using Pydantic
+    validated = [DailyRecord.model_validate(item) for item in data]
+
+    assert len(validated) == len(data)
+
+    # stronger checks
+    for record in validated:
+        assert isinstance(record.date, datetime)
+        assert isinstance(record.temperature_2m_max, float)
+        assert isinstance(record.temperature_2m_min, float)
