cb7e6a846dac9598c7fa8fbccaf258334b9e0dae
Nicholas Doboszenski <waffleer@orangeSlicer.>
2026-03-23 11:24:42 -0500

    Remove inteintal failure

--- e2e/test_database.py
+++ e2e/test_database.py
@@ -5,7 +5,3 @@ from pisense.api.main import app
 def test_connect_to_db():
     with TestClient(app): # Will run with lifecycle function
         assert True
-
-def test_fail():
-    with TestClient(app):
-        assert False
