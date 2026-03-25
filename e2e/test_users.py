6f5f3a61b840c1fc7479ae50535e4257fa087b6b
Nicholas Doboszenski <waffleer@orangeSlicer.>
2026-03-25 11:19:53 -0500

    Test get_users and its possible conditions

--- e2e/test_users.py
+++ e2e/test_users.py
@@ -4,7 +4,7 @@ from pisense.api.main import app
 from pisense.backend.classes import USER_ROLES, User, Database
 
 
-def test_create_delete_user():
+def test_create_get_users():
     with TestClient(app): # Will run with lifecycle function
         db = Database()
 
@@ -17,7 +17,7 @@ def test_create_delete_user():
         assert user.username == username
         assert user.role == role
 
-        username = "test_username_analyst_goodemail@email.com"
+        username = "test_username_analyst_email"
         role = USER_ROLES.analyst
         email = 'goodemail@email.com'
         db.create_user(username=username, role=role, email=email)
@@ -28,7 +28,7 @@ def test_create_delete_user():
         assert user.role == role
         assert user.email == email
 
-        username = "test_username_analyst_goodemail@email.com"
+        username = "test_username_viewer_email_firstname_lastname_password"
         role = USER_ROLES.viewer
         email = 'goodemail@email.com'
         firstname = 'bill'
@@ -43,3 +43,35 @@ def test_create_delete_user():
         assert user.email == email
         assert user.firstname == firstname
         assert user.lastname == lastname
+
+        users = db.get_users()
+        assert len(users) == 3
+        for user in users:
+            assert isinstance(user, User)
+            assert not isinstance(user.username, type(None))
+            assert not isinstance(user.id, type(None))
+            assert not isinstance(user.role, type(None))
+
+        users = db.get_users("test_username")
+        assert len(users) == 3
+        for user in users:
+            assert isinstance(user, User)
+            assert not isinstance(user.username, type(None))
+            assert not isinstance(user.id, type(None))
+            assert not isinstance(user.role, type(None))
+
+        users = db.get_users("email")
+        assert len(users) == 2
+        for user in users:
+            assert isinstance(user, User)
+            assert not isinstance(user.username, type(None))
+            assert not isinstance(user.id, type(None))
+            assert not isinstance(user.role, type(None))
+
+        users = db.get_users("firstname")
+        assert len(users) == 1
+        for user in users:
+            assert isinstance(user, User)
+            assert not isinstance(user.username, type(None))
+            assert not isinstance(user.id, type(None))
+            assert not isinstance(user.role, type(None))
