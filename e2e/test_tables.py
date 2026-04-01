d95261d806aeff4841285cc02ed5cf79f2d89243
Nicholas Doboszenski <waffleer@orangeSlicer.>
2026-04-01 16:32:27 -0500

    Add more order

--- e2e/test_tables.py
+++ e2e/test_tables.py
@@ -2,27 +2,33 @@
 from fastapi.testclient import TestClient
 from pisense.api.main import app
 from pisense.api.main import Database
+import pytest
 
+@pytest.mark.order(0) # global scope
 def test_get_datasets():
     with TestClient(app): # Will run with lifecycle function
         db = Database()
         datasets = db._get_rows("dataset") # noqa: F841
 
+@pytest.mark.order(0) # global scope
 def test_get_projects_table():
     with TestClient(app): # Will run with lifecycle function
         db = Database()
         projects = db._get_rows("projects") # noqa: F841
 
+@pytest.mark.order(0) # global scope
 def test_get_roles_table():
     with TestClient(app): # Will run with lifecycle function
         db = Database()
         roles = db._get_rows("roles") # noqa: F841
 
+@pytest.mark.order(0) # global scope
 def test_get_user_projects_table():
     with TestClient(app): # Will run with lifecycle function
         db = Database()
         user_projects = db._get_rows("user_projects") # noqa: F841
 
+@pytest.mark.order(0) # global scope
 def test_get_users_table():
     with TestClient(app): # Will run with lifecycle function
         db = Database()
