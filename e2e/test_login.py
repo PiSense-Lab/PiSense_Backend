f07d7a4232e86b72b5d4e05d6ee7a883c802c45e
Nicholas Doboszenski <waffleer@orangeSlicer.>
2026-04-13 11:00:52 -0500

    Fix token issue

--- e2e/test_login.py
+++ e2e/test_login.py
@@ -27,20 +27,14 @@ def test_user_login():
         out = auth.authenticate_user("test_username", password)
         assert isinstance(out, User)
 
-        access_token = auth.create_access_token({"sub": user.username}, expires_delta=timedelta(minutes=int(Authenticator().ACCESS_TOKEN_EXPIRE_MINUTES)))
-        print("Access Token")
-        print(access_token)
-        print(type(access_token))
-        assert isinstance(access_token, dict)
-
-        token = access_token["access_token"]
-        token_type = access_token["token_type"]
-
-        assert not isinstance(token, type(None))
-        assert token_type == "bearer"
+        token = auth.create_access_token({"sub": user.username}, expires_delta=timedelta(minutes=int(Authenticator().ACCESS_TOKEN_EXPIRE_MINUTES)))
+        assert isinstance(token, str)
 
         verify_out = auth.verify_token(token)
         print(verify_out)
         print(type(verify_out))
+
+        assert isinstance(verify_out, dict)
+        assert verify_out["sub"] == username
 
         assert 0 == 1
