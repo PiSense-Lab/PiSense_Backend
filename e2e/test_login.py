2b222e1062a3e08be63e244e403a234bdf7cfb8a
Nicholas Doboszenski <waffleer@orangeSlicer.>
2026-04-13 10:17:59 -0500

    Create auth token

--- e2e/test_login.py
+++ e2e/test_login.py
@@ -2,6 +2,7 @@
 from fastapi.testclient import TestClient
 from pisense.api.main import app
 from pisense.backend.classes import USER_ROLES, User, Database, Authenticator
+from datetime import timedelta
 import pytest
 
 @pytest.mark.order(1) # global scope
@@ -24,14 +25,16 @@ def test_create_get_users():
         assert user.email == email
 
         out = auth.authenticate_user("test_username", password)
-        assert not isinstance(out, type(None))
-        print(out)
-        print(type(out))
+        assert isinstance(out, User)
 
-        token = out["access_token"]
+        access_token = auth.create_access_token({"sub": user.username}, expires_delta=timedelta(minutes=int(Authenticator().ACCESS_TOKEN_EXPIRE_MINUTES)))
+        assert not isinstance(access_token, type(None))
+
+        token = access_token["access_token"]
+        token_type = access_token["token_type"]
 
         assert not isinstance(token, type(None))
-        assert out["token_type"] == "bearer"
+        assert token_type == "bearer"
 
         verify_out = auth.verify_token(token)
         print(verify_out)
