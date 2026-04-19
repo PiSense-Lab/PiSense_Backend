7e6419f322d26c8e98a90301f7fb35d806cbac8c
Neil Stephenson <131787669+Neilstephenso@users.noreply.github.com>
2026-04-19 02:00:00 -0500

    Update classes.py (#38)
    * fixing login hopefully; username - test_working, password - password

--- pisense/backend/classes.py
+++ pisense/backend/classes.py
@@ -969,13 +969,13 @@ class Database():
         columns = ["username", "email", "password", "firstname", "lastname"]
         output = [username, email, Authenticator().hash_password(password), firstname, lastname]
 
-        if firstname:
-            columns.append("firstname")
-            output.append(firstname)
+#        if firstname:
+#            columns.append("firstname")
+#            output.append(firstname)
 
-        if lastname:
-            columns.append("lastname")
-            output.append(lastname)
+#        if lastname:
+#            columns.append("lastname")
+#            output.append(lastname)
 
         user = self._insert_row("users", columns, output)
 
