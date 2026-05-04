eee67b37c66b3eaa418652a6a8f8e86ef7839647
Neilstephenso <familysteph51@outlook.com>
2026-05-04 11:32:09 -0500

    .

--- pisense/backend/routes/data_analysis.py
+++ pisense/backend/routes/data_analysis.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pisense.backend.classes import Database
 import pandas as pd
+import numpy as np
 
 router = APIRouter(prefix="/analysis")
 
@@ -17,8 +18,13 @@ async def z_detect_outliers(
     raw = db.get_table(tablename, project_id=project_id)
     undet = pd.DataFrame(raw)
     z = (undet[y_title] - undet[y_title].expanding().mean()) / undet[y_title].expanding().std()
-    print(z)
-    return z
+    z[np.isnan(z.astype(float))] = 0
+
+    res_arr = []
+    for u_val, z_score in zip(undet[y_title], z):
+        if z_score >= 2 or z_score <= -2:
+            res_arr.append(u_val)
+    return res_arr
 
 #@router.get("/mad_detect_anomalies")
 #async def mad_detect_outliers(
